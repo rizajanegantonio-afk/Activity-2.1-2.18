e50127571aea0358776f72032d6abb578ca2fac5
rizajanegantonio-afk <rizajane.g.antonio@isu.edu.ph>
2026-09-02 23:46:37 +0800

    Activity 2.16 Number.java

---
+++ Number.java
@@ -0,0 +1,14 @@
+/* Name: Riza Jane Antonio 
+Course/Year/Section: BSIT NETSEC 1-1 */
+
+public class Number 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("number\tsquare\tcube");
+        System.out.println("0\t0\t0");
+        System.out.println("1\t1\t1");
+        System.out.println("2\t4\t8");
+        System.out.println("3\t9\t27");
+    }
+}
