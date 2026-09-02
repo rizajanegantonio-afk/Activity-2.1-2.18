fd14fafac45997ee2245b5f70d170dc187c7ed28
rizajanegantonio-afk <rizajane.g.antonio@isu.edu.ph>
2026-09-02 23:35:42 +0800

    Activity 2.9 Room.java

---
+++ Room.java
@@ -0,0 +1,14 @@
+/* Name: Riza Jane Antonio 
+Course/Year/Section: BSIT NETSEC 1-1 */
+
+public class Room
+{
+    public static void main(String[] args)
+    {
+        int length = 20;
+        int width = 15;
+        int floorSpace = length*width;
+        
+        System.out.println("The floor space is " +floorSpace+ " square feet");
+    }
+}
