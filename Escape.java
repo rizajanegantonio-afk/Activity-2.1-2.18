2c4038dbc4fe774337b271a530c781323a43523e
rizajanegantonio-afk <rizajane.g.antonio@isu.edu.ph>
2026-09-02 23:44:42 +0800

    Activity 2.15 Escape.java

---
+++ Escape.java
@@ -0,0 +1,11 @@
+/* Name: Riza Jane Antonio 
+Course/Year/Section: BSIT NETSEC 1-1 */
+
+public class Escape 
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("First Name\tLast Name\tStudent Number\tPhone Number");
+        System.out.println("Riza Jane\tAntonio\t26-215\t09358284263");
+    }
+}
