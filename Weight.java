ce4faecc1c3bd3e15f1722cc36646315aa1b41b1
rizajanegantonio-afk <rizajane.g.antonio@isu.edu.ph>
2026-09-02 23:33:19 +0800

    Activity 2.8 Weight.java

---
+++ Weight.java
@@ -0,0 +1,14 @@
+/* Name: Riza Jane Antonio
+Course/year/section: BSIT NETSEC 1-1 */
+
+public class Weight
+{
+    public static void main(String[] args)
+    {
+        double pounds = 150;
+        double kilograms = pounds*0.454;
+        
+        System.out.println("Weight in pounds:" + pounds);
+        System.out.println("Weight in kilograms:" + kilograms);
+    }
+}
