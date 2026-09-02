4b198ca7e3e5dadebb22f7bca7b09877ee6b0dd9
rizajanegantonio-afk <rizajane.g.antonio@isu.edu.ph>
2026-09-02 22:40:05 +0800

    Activity 2.1 desiderata

---
+++ desiderata.java
@@ -0,0 +1,18 @@
+/* 
+Name: Riza Jane Anronio
+Course/Year/Section: BSIT NETSEC 1-1
+*/
+
+public class desiderata
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Go placidly amid the noise and haste,");
+        System.out.println("and remember what peace there may be in silence.");
+        System.out.println("As far as possible, without surrender,");
+        System.out.println("be on good terms with all persons. Speak your truth quietly and");
+        System.out.println("clearly; and listen to others, even to the dull and the ignorant,");
+        System.out.println("they too have their story.");
+        System.out.println("Avoid loud and aggressive person.");
+    }
+}
