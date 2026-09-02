5940e7ce14feb11d2578590d4edacde545604221
rizajanegantonio-afk <rizajane.g.antonio@isu.edu.ph>
2026-09-02 23:26:45 +0800

    Activity 2.5 Message.java

---
+++ Message.java
@@ -0,0 +1,16 @@
+/* Name: Riza Jane Antonio
+Course/Year/Section: BSIT NETSEC 1-1 */
+
+public class Message
+{
+    public static void main(String[] args)
+    {
+        String Message1= "I love my course";
+        String Message2= "I love Java programming";
+        String Message3= "I want to be a Java programmer";
+        
+        System.out.println(Message1);
+        System.out.println(Message2);
+        System.out.println(Message3);
+    }
+}
