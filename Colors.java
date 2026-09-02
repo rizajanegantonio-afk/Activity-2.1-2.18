ce62f7f414c4e10695c6597e9441c115f723c7e3
rizajanegantonio-afk <rizajane.g.antonio@isu.edu.ph>
2026-09-02 23:17:51 +0800

    Activity 2.3 Colors.java

---
+++ Colors.java
@@ -0,0 +1,15 @@
+public class Colors
+{
+    public static void main(String[] args)
+    {
+        String color1 = "yellow";
+        String color2 = "green";
+        String color3 = "red";
+        String color4 = "blue";
+        
+        System.out.println(color1);
+        System.out.println(color2);
+        System.out.println(color3);
+        System.out.println(color4);
+    }
+}
