0148e5e7c9601e49a592d6b68a996149a5e3c5b4
rizajanegantonio-afk <rizajane.g.antonio@isu.edu.ph>
2026-09-02 23:20:22 +0800

    Activity 2.4 SequentialNumbers.java

---
+++ SequentialNumbers.java
@@ -0,0 +1,16 @@
+/* 
+Name: Riza Jane Antonio
+Course/Year/Section: BSIT NETSEC 1-1
+*/
+public class SequentialNumbers
+{
+    public static void main(String[] args)
+    {
+        int sequence1=123;
+        int sequence2=456;
+        int sequence3=789;
+        System.out.println(sequence1);
+        System.out.println(sequence2);
+        System.out.println(sequence3);
+    }
+}
