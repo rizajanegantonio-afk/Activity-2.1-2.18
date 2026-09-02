3d4e9fddb4e64284adc100cdf9e03ff402fc5e78
rizajanegantonio-afk <rizajane.g.antonio@isu.edu.ph>
2026-09-02 23:42:03 +0800

    Activity 2.13 Sum.java

---
+++ Sum.java
@@ -0,0 +1,15 @@
+/* Name: Riza Jane Antonio 
+Course/Year/Section: BSIT NETSEC 1-1 */
+
+public class Sum
+{
+    public static void main(String[] args)
+    {
+        int num1 = 15;
+        int num2 = 25;
+        int sum = num1 + num2;
+        
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+        
+    }
+}
