8a6aefceef5e16cafd97bb155cf310a26eecebc9
rizajanegantonio-afk <rizajane.g.antonio@isu.edu.ph>
2026-09-02 23:28:57 +0800

    Activity 2.6 MessageValue.java

---
+++ MessageValue.java
@@ -0,0 +1,14 @@
+/* Name: Riza Jane Antonio
+Course/year/section: BSIT NETSEC 1-1 */
+
+public class MessageValue
+{
+    public static void main(String[] args)
+    {
+        String Message1 = "The value of pie is:";
+        Double value = 3.1415;
+        
+        System.out.println(Message1);
+        System.out.println(value);
+    }
+}
