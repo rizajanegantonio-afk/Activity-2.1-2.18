734bfe291c636575276669426c57a18d9e0561b4
rizajanegantonio-afk <rizajane.g.antonio@isu.edu.ph>
2026-09-02 23:31:26 +0800

    Activity 2.7 Circle.java

---
+++ Circle.java
@@ -0,0 +1,14 @@
+/* Name: Riza Jane Antonio
+Course/year/section: BSIT NETSEC 1-1 */
+
+public class Circle
+{
+    public static void main(String[] args)
+    {
+        int radius = 2;
+        double area = radius*radius*3.142;
+        
+        System.out.println("Radius:" + radius);
+        System.out.println("Area:" + area);
+    }
+}
