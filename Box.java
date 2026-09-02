e2eb4e323aec264a0d053aaa11511bdb42a0e191
rizajanegantonio-afk <rizajane.g.antonio@isu.edu.ph>
2026-09-02 23:47:48 +0800

    Activity 2.17 Box.java

---
+++ Box.java
@@ -0,0 +1,22 @@
+/* Name: Riza Jane Antonio 
+Course/Year/Section: BSIT NETSEC 1-1 */
+
+public class Box
+{
+    public static void main(String[] args) 
+    {
+        System.out.println("*********");
+        System.out.println("*       *");
+        System.out.println("* * * * *");
+        System.out.println("*   *   *");
+        System.out.println("*********");
+        
+        System.out.println();
+        
+        System.out.println("    *");
+        System.out.println("   * *");
+        System.out.println("  *   *");
+        System.out.println("   * *");
+        System.out.println("    *");
+    }
+}
